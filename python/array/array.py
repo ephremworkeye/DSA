9138b1cb475d24ba2f4af21546a5fc2d981474f5
Ephrem Wube <ephremworkeye@gmail.com>
2021-07-01 19:58:23 -0700

    simple array problems

--- python/array/array.py
+++ python/array/array.py
@@ -5,3 +5,33 @@ def sum_array(A):
         total += num
     return total
 print(sum_array([2, 3, 4 , 5]))
+
+# find min
+
+def min_value(A):
+    min = A[0]
+    for num in A:
+        if num < min:
+            min = num
+    return min
+
+print(min_value([5, 2, 4, 1, 9]))
+
+# find max
+def max_value(A):
+    max = A[0]
+    for num in A:
+        if num >max:
+            max = num
+    return max
+
+print(max_value([5, 2, 4, 1, 9]))
+
+# find average
+def average(A):
+    total = 0
+    for num in A:
+        total += num
+    return total / (len(A)-1)
+
+print(average([5, 2, 4, 1, 9]))
