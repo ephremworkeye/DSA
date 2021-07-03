bda1ac9a6da42e08eb12953ca0f6888217add936
Ephrem Wube <ephremworkeye@gmail.com>
2021-07-02 20:17:42 -0700

    bubble sort problem

---
+++ python/array/4bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(A):
+    for i in range(len(A), 0, -1):
+        for j in range(1, i):
+            if A[j] < A[j-1]:
+                A[j], A[j-1] = A[j-1], A[j]
+
+    return A
+
+print(bubble_sort([5, 2, 3, 1, 9, 0, 2]))
