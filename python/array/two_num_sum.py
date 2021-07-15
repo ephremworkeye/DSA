4bf2504411a36e3b59bf0b6f02f3028e748339b5
Ephrem Wube <ephremworkeye@gmail.com>
2021-07-15 00:39:58 -0700

    two num sum array problem solved

---
+++ python/array/two_num_sum.py
@@ -0,0 +1,21 @@
+# solve two sum problem with sorted array
+# this solution can be easily solved by nesting but with cost of n^2
+# and also can be solved using hash but with cost of o(n) time comlexity
+# since it is sorted array, we can use window sliding technique by checking element in two sides
+def two_num_sum(A, sum):
+    left, right = 0, len(A)-1
+    while left < right:
+        if A[left] + A[right] == sum:
+            return left, right # returning the indexd
+        # if sum of left and right > sum,  that means we need to find smaller element and decremnet 
+        elif A[left] + A[right] > sum:
+            right -= 1        
+        # if sum of left and right > sum,  that means we need to find greater element and increment 
+        else:
+            left +=1
+    # if not found 
+    return None
+
+print(two_num_sum([2, 4, 5,  7, 9, 14, 20], 12))
+
+    
