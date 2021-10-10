08a51cfceb48e98e0559e641d29768b71150c11e
Ephrem Wube <ephremworkeye@gmail.com>
2021-10-10 01:25:31 -0700

    grid traveler problem solved

---
+++ Javascript/dynamic_pro/grid_traveler.js
@@ -0,0 +1,26 @@
+// const gridTraveler = (m, n) => {
+//     if (m === 1 && n === 1) return 1;
+//     if (m ===1 || n === 0) return 0;
+//     return gridTraveler(m-1, n) + gridTraveler(m, n-1)
+// }
+
+// console.log(gridTraveler(0, 0));
+// console.log(gridTraveler(0, 1));
+// console.log(gridTraveler(1, 0));
+// console.log(gridTraveler(2, 3));
+// console.log(gridTraveler(3, 3));
+// console.log(gridTraveler(4, 3));
+
+// using memoization
+
+// const gridTraveler = (m, n, memo={}) => {
+//     const key = m + ',' + n
+//     if (key in memo) return memo[key];
+//     if (m === 1 && n === 1) return 1;
+//     if (m ===1 || n === 0) return 0;
+//     memo[key] = gridTraveler(m-1, n, memo) + gridTraveler(m, n-1, memo);
+//     return memo[key]
+// };
+
+// console.log(gridTraveler(3, 3))
+
